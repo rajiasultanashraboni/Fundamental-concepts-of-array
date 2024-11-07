d8ce84fe3a9381a0b580f21329e0fbe85034afb2
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 22:11:50 +0600

    task 2 is completed which was about push and pop

---
+++ task2.js
@@ -0,0 +1,16 @@
+// Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+var touristDestination = ["Bandarban", "katabon", "shundorban"];
+touristDestination.push("shalbon");
+console.log(touristDestination);
+
+touristDestination.push("mithamoin", "sitakundo");
+console.log(touristDestination);
+
+touristDestination.pop();
+console.log(touristDestination);
