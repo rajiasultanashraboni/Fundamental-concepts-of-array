d36e319476d74d0bb7092f1faf65311b9b058a84
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 22:03:31 +0600

    task one is completed which is about array

---
+++ task1.js
@@ -0,0 +1,11 @@
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+var fruits = ["Banana", "mango", "apple", "orange", "grape"];
+var index = fruits[3];
+console.log(index);
+fruits[2] = "jambura";
+console.log(fruits);
