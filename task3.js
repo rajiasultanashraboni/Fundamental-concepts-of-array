b2b3068e9a4522efe86e368f95e0acd90e005bf9
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 22:21:36 +0600

    task three is completed which is about includes

---
+++ task3.js
@@ -0,0 +1,17 @@
+// Checking Array Membership with ‘includes’
+// Instructions:
+
+// Create an array of books containing different book.
+
+// Use the includes method to check if the array contains a javascript book.
+
+// Print a message to the console indicating whether the element is present in the array or not.
+
+const books = ["Bangla", "English", "Math", "Science"];
+// console.log(books.includes("javascript"));
+
+if (books.includes("javascript")) {
+  console.log("this contain a javascript book");
+} else {
+  console.log("it does not contain javascript book");
+}
