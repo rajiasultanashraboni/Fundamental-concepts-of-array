7cc9b60e8d4419fcb027d15c8b14004c85bf396b
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-07 22:36:32 +0600

    task five is completed which is about concat

---
+++ task5.js
@@ -0,0 +1,14 @@
+// Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+var fruits = ["Banana", "mango", "apple", "orange", "grape"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(fruits.concat(numbers));
+
+console.log(fruits);
+console.log(numbers);
