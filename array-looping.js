aea41491321bf9bea91e73509342a16407798320
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-12 18:41:56 +0600

    Array looping using for loop

--- array-looping.js
+++ array-looping.js
@@ -4,5 +4,11 @@ const friends = ["Elon", "Bill", "Mark", "Warren"];
 
 // loop using for of
 for (const friend of friends) {
-  console.log(friend);
+  //   console.log(friend);
+}
+
+// loop using for loop
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(friends[i]);
 }
