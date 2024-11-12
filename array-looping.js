99c0928557450b5a5793ea71c0f2cff7023d7834
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-12 18:45:30 +0600

    array looping using while loop

--- array-looping.js
+++ array-looping.js
@@ -10,5 +10,13 @@ for (const friend of friends) {
 // loop using for loop
 
 for (let i = 0; i < friends.length; i++) {
+  //   console.log(friends[i]);
+}
+
+// loop using while loop
+
+let i = 0;
+while (i < friends.length) {
   console.log(friends[i]);
+  i++;
 }
