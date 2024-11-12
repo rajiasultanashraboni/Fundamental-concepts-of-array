6c81b868ba3cf4e77e5d513e0580e31a49eaad03
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-13 00:15:13 +0600

    Reversed word positions in sentence

---
+++ looping-task4.js
@@ -0,0 +1,13 @@
+// Task 4 (Hard)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+// Input:
+
+const statement = "I am a hard working person";
+const reversedWords = statement.split(" ").reverse().join(" ");
+
+console.log(reversedWords);
+
+// Output:
+
+// 'person working hard a am I'
